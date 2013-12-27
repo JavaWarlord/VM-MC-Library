7a8ade69ff4ec30aa8f968c24b77b3ca6c566405
JavaWarlord <JavaWarlord@gmail.com>
2013-12-27 09:36:32 -0500

    Added getSelection funtion to return a List of Locations

--- VM-MC-Library/src/com/valiantmarauders/minecraft/selection/SelectionManager.java
+++ VM-MC-Library/src/com/valiantmarauders/minecraft/selection/SelectionManager.java
@@ -1,5 +1,9 @@
 package com.valiantmarauders.minecraft.selection;
 
+import java.util.List;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -12,20 +16,19 @@ import org.bukkit.inventory.ItemStack;
  * 
  */
 public interface SelectionManager {
-//	/**
-//	 * Gets the {@link org.bukkit.entity.Player Player} making the selection.
-//	 * 
-//	 * @return the player
-//	 */
-//	public Player getPlayer(Selection selection);
-//
-//	/**
-//	 * Sets the {@link org.bukkit.entity.Player Player} making the selection.
-//	 * 
-//	 * @param player
-//	 *            the player
-//	 */
-//	public void setPlayer(Selection selection, Player player);
+	/**
+	 * Adds a point to the Selection. Creates a new Selection if one doesn't
+	 * already exist.
+	 * 
+	 * @param player
+	 *            the player selecting the point
+	 * 
+	 * @param wand
+	 *            the item used to make the selection
+	 * @param block
+	 *            the block selected
+	 */
+	public void addPoint(Player player, ItemStack wand, Block block);
 
 	/**
 	 * Adds a point to the Selection. Creates a new Selection if one doesn't
@@ -34,10 +37,20 @@ public interface SelectionManager {
 	 * @param player
 	 *            the player selection the point
 	 * 
-	 * @param wand
-	 *            the item used to make the selection
+	 * @param type
+	 *            the type of item used to make the selection
 	 * @param block
 	 *            the block selected
 	 */
-	public void addPoint(Player player, ItemStack wand, Block block);
+	public void addPoint(Player player, Material type, Block block);
+
+	/**
+	 * Gets a list of the points that make up the selection
+	 * 
+	 * @param player
+	 *            the player whos selection to get
+	 * 
+	 * @return the points
+	 */
+	public List<Location> getSelection(Player player);
 }
